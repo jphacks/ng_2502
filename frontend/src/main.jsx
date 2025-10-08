b024344745900dd131445e73acd85e314a3e7047
souta0812 <168318107+souta0812@users.noreply.github.com>
2025-10-08 18:52:50 +0900

    ChakraUI setup

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import './index.css'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+// 1. ChakraProvider をインポートします
+import { ChakraProvider } from '@chakra-ui/react'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    {/* 2. <App /> コンポーネント全体を <ChakraProvider> で囲みます */}
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
+  </React.StrictMode>,
 )
