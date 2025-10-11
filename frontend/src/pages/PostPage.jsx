0145dde0bf4f34afc7498c1df1aa682cef2a61b2
mn0803 <watervalley83@gmail.com>
2025-10-11 16:34:21 +0900

    add BackButton

--- frontend/src/pages/PostPage.jsx
+++ frontend/src/pages/PostPage.jsx
@@ -1,13 +1,16 @@
-import { Box, Divider, Heading, VStack } from "@chakra-ui/react";
-import { useLocation } from "react-router-dom";
+import { Box, Divider, Heading, IconButton, VStack } from "@chakra-ui/react";
+import { useLocation, useNavigate } from "react-router-dom";
 import { Post } from "../components/Post";
 import { useState } from "react";
+import { IoIosArrowBack } from "react-icons/io";
 
 export const PostPage = () => {
   const location = useLocation();
+  const navigate = useNavigate();
   const { post } = location.state || {}; // navigateから渡されたstateを取得
   const [comments, setComments] = useState([]);
 
+  const handleGoBack = () => navigate("/list");
   const handleCommentSubmit = (newComment) => {
     // 新しいコメントオブジェクトを作成
     const commentPost = {
@@ -24,6 +27,18 @@ export const PostPage = () => {
 
   return (
     <VStack spacing={4} align="stretch">
+      <Box alignSelf="flex-start" pl={2} pt={2}>
+        <IconButton
+          aria-label="Back to list page"
+          icon={<IoIosArrowBack size="24px" />}
+          onClick={handleGoBack}
+          variant="ghost"
+          isRound
+          _focus={{ boxShadow: "none", outline: "none" }}
+          border={"none"}
+          color="#80CBC4"
+        />
+      </Box>
       {post && <Post post={post} onCommentSubmit={handleCommentSubmit} />}
       <Divider />
       <Box p={4}>
