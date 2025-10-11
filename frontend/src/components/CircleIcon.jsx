0025b1f873f9171c8afcca9f49e5d41cc0ef71e7
mn0803 <watervalley83@gmail.com>
2025-10-11 11:48:35 +0900

    loginpage

--- frontend/src/components/CircleIcon.jsx
+++ frontend/src/components/CircleIcon.jsx
@@ -6,7 +6,7 @@ export const CircleIcon = ({ src, alt }) => {
       alt={alt}
       borderRadius={"full"}
       boxSize={{ base: "40px", md: "50px", lg: "60px" }} // 画面サイズに応じてアイコンのサイズを変更 (sm:モバイル, md:タブレット, lg:デスクトップ)
-      Fit="cover" // 画像のアスペクト比を維持しつつ、コンテナに収まるように調整
+      fit="cover" // 画像のアスペクト比を維持しつつ、コンテナに収まるように調整
     />
   );
 };
--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -2,15 +2,37 @@ import { VStack, Image, Box } from "@chakra-ui/react";
 import AppIcon from "../assets/AppIcon.png";
 import { InputText } from "../components/InputText";
 import { TextButton } from "../components/TextButton";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 export const LoginPage = () => {
+  const [mailAdress, setMailAdress] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleLogin = () => {
+    console.log({ mailAdress, password });
+    navigate("/profile");
+  };
+
   return (
     <Box display="flex" justifyContent="center">
       <VStack h="100vh" align="center" w="300px" spacing={4} pt={20}>
         <Image src={AppIcon} alt="アプリケーションアイコン" boxSize="120px" />
-        <InputText placeholder="メールアドレス" />
-        <InputText placeholder="パスワード" type="password" />
-        <TextButton w="100%">ログイン</TextButton>
+        <InputText
+          placeholder="メールアドレス"
+          value={mailAdress}
+          onChange={(e) => setMailAdress(e.target.value)}
+        />
+        <InputText
+          placeholder="パスワード"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          type="password"
+        />
+        <TextButton w="100%" onClick={handleLogin}>
+          ログイン
+        </TextButton>
         <TextButton w="100%">あたらしくはじめる</TextButton>
       </VStack>
     </Box>
