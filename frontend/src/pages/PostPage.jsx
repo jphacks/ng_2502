61c4476405a24c5064a24a589d24f6cfa33a264d
mn0803 <watervalley83@gmail.com>
2025-10-11 16:11:30 +0900

    postpage

--- frontend/src/pages/PostPage.jsx
+++ frontend/src/pages/PostPage.jsx
@@ -1,10 +1,42 @@
-import { Box, Heading } from "@chakra-ui/react";
+import { Box, Divider, Heading, VStack } from "@chakra-ui/react";
+import { useLocation } from "react-router-dom";
+import { Post } from "../components/Post";
+import { useState } from "react";
 
 export const PostPage = () => {
+  const location = useLocation();
+  const { post } = location.state || {}; // navigateから渡されたstateを取得
+  const [comments, setComments] = useState([]);
+
+  const handleCommentSubmit = (newComment) => {
+    // 新しいコメントオブジェクトを作成
+    const commentPost = {
+      id: `comment-${Date.now()}`, // 一意のIDを生成
+      content: newComment,
+      user: {
+        // 実際にはログインユーザーの情報を入れる
+        email: "current.user@example.com",
+        iconColor: "green",
+      },
+    };
+    setComments((prevComments) => [...prevComments, commentPost]);
+  };
+
   return (
-    <Box p={4}>
-      <Heading>コメントページ</Heading>
-      {/* 今後、ここにコメントの詳細表示や入力フォームを実装します */}
-    </Box>
+    <VStack spacing={4} align="stretch">
+      {post && <Post post={post} onCommentSubmit={handleCommentSubmit} />}
+      <Divider />
+      <Box p={4}>
+        <Heading size="md" mb={4}>
+          コメント
+        </Heading>
+        {/* コメントリストをPostコンポーネントを使って表示 */}
+        <VStack spacing={4} align="stretch">
+          {comments.map((comment) => (
+            <Post key={comment.id} post={comment} isComment={true} />
+          ))}
+        </VStack>
+      </Box>
+    </VStack>
   );
 };
