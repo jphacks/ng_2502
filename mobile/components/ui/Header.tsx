4d33c400c19fe0a079434572d81a8a14e2c8fe56
mn0803 <watervalley83@gmail.com>
2026-01-15 12:46:22 +0900

    feat: add icon in header

--- mobile/components/ui/Header.tsx
+++ mobile/components/ui/Header.tsx
@@ -5,12 +5,56 @@ import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { CircleIcon } from "./CircleIcon";
 import { MarkButton } from "./MarkButton";
 
+// アイコンマッピング（React Nativeではrequireを使用）
+const iconMap = {
+  blue: {
+    src: require("../../assets/images/UserIcon_Blue.png"),
+    alt: "Blue Icon",
+  },
+  cream: {
+    src: require("../../assets/images/UserIcon_Cream.png"),
+    alt: "Cream Icon",
+  },
+  green: {
+    src: require("../../assets/images/UserIcon_Green.png"),
+    alt: "Green Icon",
+  },
+  mint: {
+    src: require("../../assets/images/UserIcon_Mint.png"),
+    alt: "Mint Icon",
+  },
+  navy: {
+    src: require("../../assets/images/UserIcon_Navy.png"),
+    alt: "Navy Icon",
+  },
+  olive: {
+    src: require("../../assets/images/UserIcon_Olive.png"),
+    alt: "Olive Icon",
+  },
+  purple: {
+    src: require("../../assets/images/UserIcon_Purple.png"),
+    alt: "Purple Icon",
+  },
+  red: {
+    src: require("../../assets/images/UserIcon_Red.png"),
+    alt: "Red Icon",
+  },
+  yellow: {
+    src: require("../../assets/images/UserIcon_Yellow.png"),
+    alt: "Yellow Icon",
+  },
+};
+
+type IconColor = keyof typeof iconMap;
+
 type Props = {
   title?: string;
   onPressTutorial?: () => void;
   onPressCreate?: () => void;
   onPressLogo?: () => void;
-  iconSrc: any;
+  onPressProfile?: () => void;
+  iconColor?: IconColor;
+  iconSrc?: any;
 };
 
 export const Header: React.FC<Props> = ({
@@ -18,9 +62,18 @@ export const Header: React.FC<Props> = ({
   onPressTutorial,
   onPressCreate,
   onPressLogo,
+  onPressProfile,
+  iconColor = "blue",
   iconSrc,
 }) => {
   const insets = useSafeAreaInsets();
+
+  // iconColorが指定されている場合はiconMapから取得、iconSrcが指定されている場合はそれを使用
+  const { src, alt } =
+    iconColor && iconMap[iconColor]
+      ? iconMap[iconColor]
+      : { src: iconSrc || iconMap.blue.src, alt: "user" };
+
   return (
     <XStack
       height={72}
@@ -31,7 +84,9 @@ export const Header: React.FC<Props> = ({
       backgroundColor="$background"
       position="relative"
     >
-      <CircleIcon src={iconSrc} alt="user" />
+      <Pressable onPress={onPressProfile}>
+        <CircleIcon src={src} alt={alt} />
+      </Pressable>
       <XStack
         position="absolute"
         left={0}
