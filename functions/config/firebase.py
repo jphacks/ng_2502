bd1d949060d61adfb0001f9b7489dcbaa81de9da
yoneda-yukino <snow.12.weather@gmail.com>
2026-01-20 22:12:22 +0900

    修正

--- functions/config/firebase.py
+++ functions/config/firebase.py
@@ -1,7 +1,7 @@
-import os
-import json
 import firebase_admin
 from firebase_admin import credentials, firestore
+import os
+import json
 import io
 
 db = None
@@ -11,24 +11,24 @@ def init_firebase():
 
     cred = None
 
+    # ローカルの serviceAccountKey.json
     if os.path.exists("functions/config/serviceAccountKey.json"):
         cred = credentials.Certificate("functions/config/serviceAccountKey.json")
+
     else:
+        # Render の環境変数
         cred_json_str = os.environ.get("GOOGLE_CREDENTIALS_JSON")
         if cred_json_str:
             cred_info = json.loads(cred_json_str)
             cred = credentials.Certificate(cred_info)
         else:
-            print("⚠️ サービスアカウントキーが見つかりません。エミュレータモードで動作します。")
+            print("⚠️ サービスアカウントキーが見つかりません")
+            return
 
     try:
         firebase_admin.initialize_app(cred)
     except ValueError:
         pass
 
-    if os.getenv("FIRESTORE_EMULATOR_HOST"):
-        print("🔥 Firestore Emulator に接続しています")
-        db = firestore.Client(project="myfirstfirebase-440d6")
-    else:
-        print("⚠️ 本番 Firestore に接続しています")
-        db = firestore.client()
+    print("🔥 Firestore に接続成功")
+    db = firestore.client()
